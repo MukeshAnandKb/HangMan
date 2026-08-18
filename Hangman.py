f0488a59657d670497e901c8ce0fcb8d0b11e803
mukesh <mukeshanandkb@gmail.com>
2026-08-18 22:57:46 +0530

    Refactor game logic to improve readability and maintainability; update clue handling and game over conditions in Hangman

--- Hangman.py
+++ Hangman.py
@@ -70,38 +70,32 @@ print(''' ____   ____       _____  _____   ______         _____         ______  
   \(     )/    \(      )/     \(       )/     \( |____|/      \(          )/       \(      )/     \(       )/  
    '     '      '      '       '       '       '   )/          '          '         '      '       '       '   
                                     '                                                           ''')
-#umported from words.py
+                                                      #imported from words.py on the project directory
 target_word=random.choice(words.words)
-word_index_position=words.words.index(target_word)#to get the index position where the word is in the words list so we can get the corresponding clue
-clue=words.clues[word_index_position]
-chances_till_game_lost=0
-x=len(target_word)
-guessed=[0 for i in range(x)] #create an list full of zeroes as per number of letters in target_word -->0 is replaced by correctly guessed letter
-game_over=False
-#game_over set to false for while loop
-letters=[i for i in target_word]
+word_index_position=words.words.index(target_word)    #to get the index position where the word is in the words list from words.py so we can get the corresponding clue for this word
+clue=words.clues[word_index_position]                 #to save the clue of corresponding word on this variable to print it later down the line
+chances_till_game_lost=0                              #total 6 chances
+length_of_the_target_word=len(target_word)            
+guessed=["_" for i in range(length_of_the_target_word)]#create an list full of _ as per number of letters in target_word -->0 is replaced by correctly guessed letter
+game_over=False                                        #game_over set to false for while loop
+letters=[i for i in target_word]                       #list comprenhension list of letters of target_word
 print("the clue for the given word is :",clue)
 while(not game_over): 
-    guess_letter=input("guess an letter:").lower()#so no matter what user gives caps letter or small , the input is taken as small letter because thats how it is on the words list on words.py
+    guess_letter=input("guess an letter:").lower()    #so no matter what user gives caps letter or small , the input is taken as small letter because thats how it is on the words list on words.py
     if guess_letter in letters:
         n=letters.index(guess_letter)
         guessed[n]=guess_letter
-        letters[n]=-1#because letters in the letters list can be repeated , to account to repeaticility
+        letters[n]=-1                                 #because letters in the letters list can be repeated , to account to repeaticility
         for i in guessed:
-           if i==0: 
-            print("_",end=" ")  #to simulate _ _ _ _ _ a _ for example
-           else:
-            print(i,end=" ")
-        if 0 not in guessed:#condition to check if all the blanks are filled
-           print("GAME OVER YOU WON")
-           game_over=True
-        if not game_over:  #to skip rest of current iteration of the while loop use continue keyword
-          continue
+           print(i,end=" ")
+        if "_" not in guessed:                        #condition to check if all the blanks are filled(Winning criteria)
+           print("\n\nGAME OVER YOU WON")             #\n is the newline charecter
+           game_over=True                             #loop stop condition
     else:
-       chances_till_game_lost+=1 #total 6 chances in an hangman game
+       chances_till_game_lost+=1                      #total 6 chances in an hangman game
        lose_image(chances_till_game_lost)
        if chances_till_game_lost==6:
           print("GAME OVER YOU LOST")
           print("\n\nTHE WORD WAS",target_word)
-          break
-       continue
+          game_over=True                              #loop stop condition
+      
